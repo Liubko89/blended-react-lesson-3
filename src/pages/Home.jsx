1cdafc7c53e554bd1f99dbfc150a182c49a27ac6
YuriiRogovets <yura.rogovets@gmail.com>
2024-03-23 16:10:01 +0200

    added fechData

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,34 @@
-import { Container, Heading, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from 'service/countryApi';
 
 const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoader(true);
+
+      try {
+        const data = await getCountries();
+        setCountries(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoader(false);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Section>
       <Container>
-        <Heading title="Home" bottom />
+        {loader && <Loader />}
+        {error && <Heading title="Something went wrong ..." bottom />}
+        {countries.length > 0 && <CountryList countries={countries} />}
       </Container>
     </Section>
   );
