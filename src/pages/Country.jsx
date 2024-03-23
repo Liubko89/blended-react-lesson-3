880589c8e891f25f6202f4468f5365510f6977db
DenisBahuta <denisboguto@gmail.com>
2024-03-23 15:53:12 +0100

    bahuta-branch

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,48 @@
-import { Container, Heading, Section } from 'components';
+import {
+  Container,
+  CountryInfo,
+  GoBackBtn,
+  Heading,
+  Loader,
+  Section,
+} from 'components';
+import { useEffect, useRef, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/countryApi';
 
 const Country = () => {
+  const [country, setCountry] = useState(null);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(null);
+  const { countryId } = useParams();
+
+  const location = useLocation();
+  console.log(location, country);
+  const goBack = useRef(location?.state?.from ?? '/');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoader(true);
+
+      try {
+        const data = await fetchCountry(countryId);
+        setCountry(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoader(false);
+      }
+    };
+    fetchData();
+  }, [countryId]);
+  console.log(country);
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        <GoBackBtn path={goBack.current}>Back to countries</GoBackBtn>
+        {loader && <Loader />}
+        {error && <Heading title="Something went wrong ..." bottom />}
+        {country && <CountryInfo {...country} />}
       </Container>
     </Section>
   );
